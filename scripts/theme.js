725faa2cf7498b3c5ba98e3b40b428d753265cfd
Euan <33450935+euandeas@users.noreply.github.com>
2022-05-12 15:33:40 +0100

    Implemented 404 and placeholder homepage

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,5 +1,8 @@
 const btn = document.querySelector(".btn-toggle");
 const icon = document.querySelector(".theme-icon");
+const linkedin = document.getElementById("linkedinicon");
+const instagram = document.getElementById("instaicon");
+const github = document.getElementById("githubicon");
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
 const currentTheme = localStorage.getItem("theme");
@@ -10,9 +13,15 @@ if (currentTheme == "dark") {
 }
 if (currentTheme == "dark"){
   icon.src = "assets/moon.svg";
+  linkedin.src = "assets/linkedindark.svg";
+  instagram.src = "assets/instagramdark.svg";
+  github.src = "assets/githubdark.svg";
 }
 else{
   icon.src = "assets/sun.svg";
+  linkedin.src = "assets/linkedin.svg";
+  instagram.src = "assets/instagram.svg";
+  github.src = "assets/github.svg";
 }
 
 btn.addEventListener("click", function () {
@@ -30,8 +39,14 @@ btn.addEventListener("click", function () {
   localStorage.setItem("theme", theme);
   if (theme == "dark"){
     icon.src = "assets/moon.svg";
+    linkedin.src = "assets/linkedindark.svg";
+    instagram.src = "assets/instagramdark.svg";
+    github.src = "assets/githubdark.svg";
   }
   else{
     icon.src = "assets/sun.svg";
+    linkedin.src = "assets/linkedin.svg";
+    instagram.src = "assets/instagram.svg";
+    github.src = "assets/github.svg";
   }
 });
