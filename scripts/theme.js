c8832a669f9e21987429e1dc67e110f2ef1eafc4
Euan Deas <33450935+euandeas@users.noreply.github.com>
2022-08-08 14:56:36 +0100

    tweaks

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,4 +1,5 @@
 const btn = document.querySelector(".btn-toggle");
+const btn2 = document.querySelector(".btn-toggle2");
 const icon = document.querySelector(".theme-icon");
 const icon2 = document.querySelector(".theme-icon2");
 const logo = document.getElementById("logo");
@@ -67,6 +68,41 @@ else{
 }
 
 btn.addEventListener("click", function () {
+  if (prefersDarkScheme.matches) {
+    document.body.classList.toggle("light-theme");
+    var theme = document.body.classList.contains("light-theme")
+      ? "light"
+      : "dark";
+  } else {
+    document.body.classList.toggle("dark-theme");
+    var theme = document.body.classList.contains("dark-theme")
+      ? "dark"
+      : "light";
+  }
+  localStorage.setItem("theme", theme);
+  if (theme == "dark"){
+    logo.src = "/assets/logowcb.png"
+    logo2.src = "/assets/logowcb.png"
+    icon.src = "/assets/moon.svg";
+    icon2.src = "/assets/moon.svg";
+    linkedin.src = "/assets/linkedindark.svg";
+    instagram.src = "/assets/instagramdark.svg";
+    github.src = "/assets/githubdark.svg";
+    menu.src = "/assets/menudark.svg"
+  }
+  else{
+    logo.src = "/assets/logocb.png"
+    logo2.src = "/assets/logocb.png"
+    icon.src = "/assets/sun.svg";
+    icon2.src = "/assets/sun.svg";
+    linkedin.src = "/assets/linkedin.svg";
+    instagram.src = "/assets/instagram.svg";
+    github.src = "/assets/github.svg";
+    menu.src = "/assets/menu.svg"
+  }
+});
+
+btn2.addEventListener("click", function () {
   if (prefersDarkScheme.matches) {
     document.body.classList.toggle("light-theme");
     var theme = document.body.classList.contains("light-theme")
