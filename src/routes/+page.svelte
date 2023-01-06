c164c2db616eeb848c3d650886abf73e260cf4ff
Euan Deas <33450935+euandeas@users.noreply.github.com>
2023-01-06 16:12:03 +0000

    Added Inter as default font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="h-screen grid place-content-center bg-gray-300">
+    <h1 class="text-5xl text-blue-600">Welcome to SvelteKit</h1>
+    <p class="text-xl text-red-500">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
