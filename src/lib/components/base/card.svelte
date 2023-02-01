de7336446560e4bc740dea155d44c43986ba5df7
Euan Deas <33450935+euandeas@users.noreply.github.com>
2023-02-01 11:53:00 +0000

    Started wok on home sections

--- src/lib/components/base/card.svelte
+++ src/lib/components/base/card.svelte
@@ -1,5 +1,5 @@
 <div
-	class="block border border-gray-200 dark:border-gray-900 max-w-5xl mx-auto p-6 m-5 bg-white rounded-lg shadow-md dark:bg-black"
+	class="border border-gray-200 dark:border-gray-900 bg-white lg:rounded-lg shadow-md dark:bg-black px-3 py-2"
 >
 	<slot />
 </div>
---
+++ src/lib/components/base/cardsection.svelte
@@ -0,0 +1,5 @@
+<div
+	class="block border border-gray-200 dark:border-gray-900 max-w-5xl mx-auto p-6 m-5 bg-white lg:rounded-lg shadow-md dark:bg-black"
+>
+	<slot />
+</div>
